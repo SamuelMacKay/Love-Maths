93319205c5160cf26e5ecbe62c052bdcb9ce46d4
Samuel MacKay <samuel.john.mackay@gmail.com>
2024-02-26 08:39:44 +0000

    fixed addidtion game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Wait for the DOM to finish loading before starting the game
+// Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -19,14 +19,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
-
 /**
- * The main game "loop", called when the script is first loaded 
- * and after the user 's answer has been processed 
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
  */
-function runGame(gameType){
+function runGame(gameType) {
 
-// Creates random number from 1>25
+    // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
@@ -34,44 +33,39 @@ function runGame(gameType){
         displayAdditionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. aborting!`;
+        throw `Unknown game type: ${gameType}. Aborting!`;
     }
 
+}
+
+function checkAnswer() {
 
 }
 
-function checkAnswer(){
+function calculateCorrectAnswer() {
 
 }
 
-function calculateCorrectAnswer(){
+function incrementScore() {
 
 }
 
-function incrementScore(){
+function incrementWrongAnswer() {
 
 }
 
-function incrementWrongAnswer(){
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+    
+}
+
+function displaySubtractQuestion() {
 
 }
 
-function displayAdditionQuestion(operand1, operand2){
-    document.getElementById(operand1).textContent = operand1;
-    document.getElementById(operand2).textContent = operand2;
-    document.getElementById(operator).textContent = "+";
-
-
+function displayMultiplyQuestion() {
+    
 }
-
-function displaySubtractQuestion(){
-
-}
-
-function displayMultiplyQuestion(){
-
-}
-
-// function displayDivideQuestion(){
-
-// }
